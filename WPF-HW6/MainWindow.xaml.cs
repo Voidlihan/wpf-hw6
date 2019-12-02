bd3f0c3a08a8f540ae4187b08e15f50ca8bc7925
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-02 17:01:14 +0600

    доделование

--- WPF-HW6/MainWindow.xaml.cs
+++ WPF-HW6/MainWindow.xaml.cs
@@ -27,7 +27,97 @@ namespace WPF_HW6
 
         private void BuyAll(object sender, RoutedEventArgs e)
         {
+            int sum = 0;
+            sum -= Convert.ToInt32(dataGridSum.Items[1]);
 
+        }
+
+        private void Benzin92(object sender, RoutedEventArgs e)
+        {
+            int price = 100;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBox92.Text = sum.ToString();
+        }
+
+        private void Benzin96(object sender, RoutedEventArgs e)
+        {
+            int price = 200;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBox96.Text = sum.ToString();
+        }
+
+        private void ButtonNeskvik(object sender, RoutedEventArgs e)
+        {
+            int price = 50;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBoxNeskvik.Text = sum.ToString();
+        }
+
+        private void ButtonSnikers(object sender, RoutedEventArgs e)
+        {
+            int price = 50;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBoxSnikers.Text = sum.ToString();
+        }
+
+        private void Button5liter(object sender, RoutedEventArgs e)
+        {
+            int price = 70;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBox5liter.Text = sum.ToString();
+        }
+
+        private void Button2liter(object sender, RoutedEventArgs e)
+        {
+            int price = 30;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBox2liter.Text = sum.ToString();
+        }
+
+        private void GlassClean(object sender, RoutedEventArgs e)
+        {
+            int price = 80;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBoxGlassClean.Text = sum.ToString();
+        }
+
+        private void Diamond(object sender, RoutedEventArgs e)
+        {
+            int price = 90;
+            int sum = price;
+            for (int i = 0; i < dataGridSum.Items.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridSum.Items[i]);
+            }
+            textBoxDiamond.Text = sum.ToString();
         }
     }
 }
